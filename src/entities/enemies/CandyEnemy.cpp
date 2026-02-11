2467e9220169d454b6ef4d0357e9bab5343e09f8
Jackob2004 <zjakub2004@gmail.com>
2026-02-11 12:02:11 +0100

    Define and implement candy enemy

---
+++ src/entities/enemies/CandyEnemy.cpp
@@ -0,0 +1,55 @@
+#include "CandyEnemy.h"
+
+#include "core/TextureManager.h"
+#include "entities/enemies/states/CandyEnemyState.h"
+#include "entities/enemies/states/CandyAdvancingState.h"
+
+CandyEnemy::CandyEnemy() :
+    Entity({0, 0, 50, 50},
+           new SpriteRenderer(4, TextureManager::getTexture("assets/enemies/candy_enemy.png")),
+           {0, 0}),
+    health(100) {
+    state = &CandyEnemyState::advancing;
+    state->enter(*this);
+}
+
+void CandyEnemy::update() {
+    if (!isAlive()) return;
+    Entity::update();
+    getRenderer()->advanceSprite();
+    stateTimeLeft--;
+
+    if (CandyEnemyState* updatedState = state->update(*this); updatedState != nullptr) {
+        state = updatedState;
+        state->enter(*this);
+    }
+}
+
+void CandyEnemy::render() {
+    if (!isAlive()) return;
+    Entity::render();
+}
+
+void CandyEnemy::setTarget(Player* player) {
+    target = player;
+}
+
+bool CandyEnemy::isAlive() const {
+    return health > 0;
+}
+
+void CandyEnemy::setStateTimeLeft(const int time) {
+    stateTimeLeft = time;
+}
+
+int CandyEnemy::getStateTimeLeft() const {
+    return stateTimeLeft;
+}
+
+void CandyEnemy::setState(CandyEnemyState* updatedState) {
+    state = updatedState;
+}
+
+void CandyEnemy::setHealth(const int updatedHealth) {
+    health = updatedHealth;
+}
---
+++ src/entities/enemies/CandyEnemy.h
@@ -0,0 +1,43 @@
+#ifndef SPACESHOOTER_CANDYENEMY_H
+#define SPACESHOOTER_CANDYENEMY_H
+
+#include "core/events/Subject.h"
+#include "entities/Entity.h"
+#include "entities/Player.h"
+class CandyEnemyState;
+
+class CandyEnemy : public Entity, public Subject<Vector2> {
+    friend class CandyShootingState;
+    friend class CandyTargetingState;
+
+public:
+    CandyEnemy();
+
+    void update() override;
+
+    void render() override;
+
+    void init(Vector2 spawnPoint) override {
+    }
+
+    void setTarget(Player* player);
+
+    [[nodiscard]] bool isAlive() const override;
+
+    void setStateTimeLeft(int time);
+
+    [[nodiscard]] int getStateTimeLeft() const;
+
+    void setState(CandyEnemyState* updatedState);
+
+    void setHealth(int updatedHealth);
+
+private:
+    Player *target = nullptr;
+    unsigned int health;
+    CandyEnemyState* state;
+    int stateTimeLeft;
+};
+
+
+#endif //SPACESHOOTER_CANDYENEMY_H
