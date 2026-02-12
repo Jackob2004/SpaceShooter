45ac5eb966882f71db5977182bda988e15b80e22
Jackob2004 <zjakub2004@gmail.com>
2026-02-12 14:05:34 +0100

    Define Damageable interface
    Will help with defining behavior on collisions.

---
+++ src/entities/Damageable.h
@@ -0,0 +1,15 @@
+#ifndef SPACESHOOTER_DAMAGEABLE_H
+#define SPACESHOOTER_DAMAGEABLE_H
+
+
+class Damageable {
+public:
+    virtual ~Damageable() = default;
+
+    virtual void dealDamage(int damage) = 0;
+
+    virtual int getDamage() = 0;
+};
+
+
+#endif //SPACESHOOTER_DAMAGEABLE_H
