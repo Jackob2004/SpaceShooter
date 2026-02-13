f808669dad9fb9c35d898e9fb41e590f4b6dd359
Jackob2004 <zjakub2004@gmail.com>
2026-02-13 13:37:15 +0100

    Add new functions to MathUtils

--- src/utils/MathUtils.cpp
+++ src/utils/MathUtils.cpp
@@ -6,9 +6,17 @@
 #include "core/Game.h"
 
 namespace MathUtils {
-bool isInRange(const Vector2 it, const Vector2 targetPosition, const float range) {
+bool isInRange(const float it, const float targetPosition, const float range, const int maxRange) {
     assert(range < 1.0 && range > 0.0 && "Range must be between 0 and 1");
-    return std::abs(targetPosition.x - it.x) <= (SCREEN_WIDTH * range);
+    return std::abs(targetPosition - it) <= static_cast<float>(maxRange) * range;
+}
+
+bool isInVerticalRange(const Vector2 it, const Vector2 targetPosition, const float range) {
+    return isInRange(it.y, targetPosition.y, range, SCREEN_HEIGHT);
+}
+
+bool isInHorizontalRange(const Vector2 it, const Vector2 targetPosition, const float range) {
+    return isInRange(it.x, targetPosition.x, range, SCREEN_WIDTH);
 }
 
 int getRandomInRange(const int from, const int to) {
@@ -19,5 +27,28 @@ int getRandomInRange(const int from, const int to) {
     std::uniform_int_distribution distrib(from, to);
 
     return distrib(gen);
+}
+
+Vector2 normalizeVector(const Vector2 vector) {
+    const float magnitude = std::sqrt(vector.x * vector.x + vector.y * vector.y);
+
+    return {
+        .x = vector.x / magnitude,
+        .y = vector.y / magnitude
+    };
+}
+
+Vector2 subtrackVectors(const Vector2 a, const Vector2 b) {
+    return {
+        .x = a.x - b.x,
+        .y = a.y - b.y
+    };
+}
+
+Vector2 scaleVector(const Vector2 a, const float scalar) {
+    return {
+        .x = a.x * scalar,
+        .y = a.y * scalar
+    };
 }
 }
--- src/utils/MathUtils.h
+++ src/utils/MathUtils.h
@@ -3,9 +3,17 @@
 #include <raylib.h>
 
 namespace MathUtils {
-bool isInRange(Vector2 it, Vector2 targetPosition, float range);
+bool isInVerticalRange(Vector2 it, Vector2 targetPosition, float range);
+
+bool isInHorizontalRange(Vector2 it, Vector2 targetPosition, float range);
 
 int getRandomInRange(int from, int to);
+
+Vector2 normalizeVector(Vector2 vector);
+
+Vector2 subtrackVectors(Vector2 a, Vector2 b);
+
+Vector2 scaleVector(Vector2 a, float scalar);
 }
 
 #endif //SPACESHOOTER_MATHUTILS_H
