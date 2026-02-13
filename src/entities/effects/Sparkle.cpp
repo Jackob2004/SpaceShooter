96e68323f9da86f2bb3488590420c325df2fe796
Jackob2004 <zjakub2004@gmail.com>
2026-02-13 15:46:48 +0100

    Add Sparkle effect
    Define and implement new entity effect.

---
+++ src/entities/effects/Sparkle.cpp
@@ -0,0 +1,48 @@
+#include "Sparkle.h"
+
+#include "core/Game.h"
+#include "core/TextureManager.h"
+
+Sparkle::Sparkle():
+    Entity({0, 0, 80, 80},
+           new SpriteRenderer(FRAMES, TextureManager::getTexture("assets/effects/sparkle.png")),
+           {0, 0}),
+    timeLeft(0),
+    updatesCounter(0) {
+}
+
+void Sparkle::init(const Vector2 spawnPoint) {
+    setPosition(spawnPoint);
+    constexpr int LIFE_CYCLES = 4;
+    timeLeft = (FPS / FRAMES_SPEED * FRAMES) * LIFE_CYCLES;
+}
+
+void Sparkle::update() {
+    if (!isAlive()) return;
+    Entity::update();
+    getRenderer()->advanceSprite();
+
+    timeLeft--;
+    updatesCounter++;
+}
+
+void Sparkle::render() {
+    if (!isAlive()) return;
+    Entity::render();
+}
+
+bool Sparkle::isAlive() const {
+    return timeLeft > 0;
+}
+
+void Sparkle::dealDamage(int damage) {
+}
+
+int Sparkle::getDamage() {
+    if (updatesCounter > DAMAGE_COOLDOWN) {
+        updatesCounter = 0;
+        return 10;
+    }
+
+    return 0;
+}
---
+++ src/entities/effects/Sparkle.h
@@ -0,0 +1,31 @@
+#ifndef SPACESHOOTER_SPARKLE_H
+#define SPACESHOOTER_SPARKLE_H
+#include "entities/Damageable.h"
+#include "entities/Entity.h"
+
+
+class Sparkle : public Entity, public Damageable {
+public:
+    Sparkle();
+
+    void init(Vector2 spawnPoint) override;
+
+    void update() override;
+
+    void render() override;
+
+    [[nodiscard]] bool isAlive() const override;
+
+    void dealDamage(int damage) override;
+
+    int getDamage() override;
+
+private:
+    static constexpr int FRAMES = 5;
+    static constexpr int DAMAGE_COOLDOWN = 60;
+    int timeLeft;
+    int updatesCounter;
+};
+
+
+#endif //SPACESHOOTER_SPARKLE_H
