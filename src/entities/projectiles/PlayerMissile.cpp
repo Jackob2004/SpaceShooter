520481d34c099caa6f0775b23efc84d2a4f981ca
Jackob2004 <zjakub2004@gmail.com>
2026-02-15 13:36:03 +0100

    Add PlayerMissile class

---
+++ src/entities/projectiles/PlayerMissile.cpp
@@ -0,0 +1,54 @@
+#include "PlayerMissile.h"
+
+#include "core/Game.h"
+#include "core/TextureManager.h"
+#include "utils/MathUtils.h"
+
+PlayerMissile::PlayerMissile() :
+    Entity({0, 0, 50, 50},
+           new SpriteRenderer(2, TextureManager::getTexture("assets/player/projectiles/player_missile.png")),
+           {0, VELOCITY_Y}),
+    alive(false) {
+}
+
+void PlayerMissile::init(const Vector2 spawnPoint) {
+    setPosition(spawnPoint);
+    alive = true;
+}
+
+void PlayerMissile::update() {
+    if (!isAlive()) return;
+    Entity::update();
+    getRenderer()->advanceSprite();
+
+    setVelocity({MathUtils::computeOscillationSpeed(AMPLITUDE, FREQUENCY, lifeTime), VELOCITY_Y});
+    lifeTime += GetFrameTime();
+
+    if (Game::isOutOfVerticalBounds(getPosition())) {
+        alive = false;
+    }
+}
+
+void PlayerMissile::render() {
+    if (!isAlive()) return;
+    Entity::render();
+}
+
+bool PlayerMissile::isAlive() const {
+    return alive;
+}
+
+void PlayerMissile::takeDamage(int damage) {
+    if (!isAlive()) return;
+    alive = false;
+
+    const Vector2 spawnPoint = {
+        .x = getPosition().x,
+        .y = getPosition().y - getEntityHeight()
+    };
+    notify(spawnPoint, PLAYER_MISSILE_EXPLODED);
+}
+
+int PlayerMissile::getDamage() {
+    return 0;
+}
---
+++ src/entities/projectiles/PlayerMissile.h
@@ -0,0 +1,32 @@
+#ifndef SPACESHOOTER_PLAYERMISSILE_H
+#define SPACESHOOTER_PLAYERMISSILE_H
+#include "core/events/Subject.h"
+#include "entities/Damageable.h"
+#include "entities/Entity.h"
+
+class PlayerMissile : public Entity, public Damageable, public Subject<Vector2> {
+public:
+    PlayerMissile();
+
+    void init(Vector2 spawnPoint) override;
+
+    void update() override;
+
+    void render() override;
+
+    [[nodiscard]] bool isAlive() const override;
+
+    void takeDamage(int damage) override;
+
+    int getDamage() override;
+
+private:
+    static constexpr float AMPLITUDE = 15;
+    static constexpr float FREQUENCY = 5;
+    static constexpr float VELOCITY_Y = -160;
+    float lifeTime = 0;
+    bool alive;
+};
+
+
+#endif //SPACESHOOTER_PLAYERMISSILE_H
